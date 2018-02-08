9f04e5dbdef33b17937cd627aad6292f24ae6b8f
Cameron Mkj <cameronmkj@gmail.com>
2018-02-08 21:29:27 +0000

    Script now fully functional

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import subprocess, time, webbrowser, keyboard, os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 import mkj_pkg.main as mkj
-
-while keyboard.is_pressed('ins'):
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening... ")
-            audio = r.listen(source)
-            BING_KEY = "901ff8bec4f04c679b9680be6ebf10f0"  #API KEY HERE
-            word = r.recognize_bing(audio, key=BING_KEY)
-            wordS = word.lower()
-            print(wordS)
+while True:
+    while keyboard.is_pressed('ins'):
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Listening... ")
+                audio = r.listen(source)
+                BING_KEY = ""  #API KEY HERE
+                word = r.recognize_bing(audio, key=BING_KEY)
+                print(word)
+                mkj.ty(word)
+                mkj.pr(Key.enter)
